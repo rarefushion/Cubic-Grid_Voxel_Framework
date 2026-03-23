f6a13d46385283819c8cf3fd6e725b0a41fc031f
galen <rarefushion@gmail.com>
2026-03-23 05:13:08 -0700

    Feat: ChunkCluster
    Create ChunkCluster to easily and efficiently mange chunks.

---
+++ Framework/ChunkCluster.cs
@@ -0,0 +1,56 @@
+using System.Numerics;
+using Silk.NET.Maths;
+using static GalensUnified.CubicGrid.Core.Math.RegionMath;
+
+/// <summary>Manages a specified number of chunks allowing retrieval via their position.</summary>
+public class ChunkCluster
+{
+    public readonly int chunkLength;
+    public readonly int chunkVolume;
+    public readonly int chunkCount;
+    public readonly int clusterChunkLength;
+    public readonly int clusterLength;
+    public readonly int blockCount;
+
+    private readonly ushort[] flattenedChunks;
+
+    /// <summary>Fetches a chunk (Span<ushort>) that can be directly modified.</summary>
+    private Span<ushort> GetChunkByIndex(int index) =>
+        flattenedChunks.AsSpan(index, chunkVolume);
+
+    /// <summary>Fetches a chunk (Span<ushort>) that can be directly modified.</summary>
+    public Span<ushort> GetChunkByPosition(Vector3D<int> pos) =>
+        GetChunkByIndex(IndexByChunkCoord(ChunkCoordByGlobalPos(pos)));
+
+    /// <summary>Sets the entire specified chunk to Air.</summary>
+    public void RemoveChunk(Vector3D<int> pos) =>
+        GetChunkByPosition(pos).Clear();
+
+    /// <summary>
+    /// Calculates the chunk coordinate (grid address) by dividing a position by the chunk size.
+    /// First wrapping the position into the local world space.
+    /// </summary>
+    public Vector3D<int> ChunkCoordByGlobalPos(Vector3D<int> pos) =>
+        ChunkCoordByLocalPos(LocalPosByGlobalPos(pos, clusterLength));
+
+    /// <summary>Calculates the chunk coordinate (grid address) by dividing a position by the chunk size.</summary>
+    public Vector3D<int> ChunkCoordByLocalPos(Vector3D<int> pos) =>
+        pos / chunkLength;
+
+    /// <summary>Calculates the 1D index of a chunk coordinate (grid address).</summary>
+    public int IndexByChunkCoord(Vector3D<int> coord) =>
+        ((coord.Z * clusterChunkLength + coord.Y) * clusterChunkLength + coord.X) * chunkVolume;
+
+    /// <param name="chunkLength">The length of a single chunk. In other words the cube root of the chunk's volume.</param>
+    /// <param name="clusterChunkLength">Number of chunks along each axis, allowing for a non cubic cluster.</param>
+    public ChunkCluster(int chunkLength, int clusterChunkLength)
+    {
+        this.chunkLength = chunkLength;
+        this.chunkVolume = chunkLength * chunkLength * chunkLength;
+        this.clusterChunkLength = clusterChunkLength;
+        this.chunkCount = clusterChunkLength * clusterChunkLength * clusterChunkLength;
+        this.clusterLength = clusterChunkLength * chunkLength;
+        this.blockCount = chunkVolume * chunkCount;
+        this.flattenedChunks = new ushort[blockCount];
+    }
+}
