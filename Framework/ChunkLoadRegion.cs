f20ab9fb78676fa4ef776a5348dc6e66f7e7017f
galen <rarefushion@gmail.com>
2026-04-03 19:44:53 -0700

    Fix: ChunkLoadRegion Starting Values and Updates
    ChunkLoadRegion didn't properly set it's starting values, fixed that.
    ChunkLoadRegion wasn't finding all chunks because it wasn't using the
    proper distance, fixed by multiplying HalfLengthInChunks by chunkLength.
    ChunkLoadRegion would invoke ChunkAdded even for chunks that were
    already accounted for, fixed by checking if they were already in the
    known chunk list.

--- Framework/ChunkLoadRegion.cs
+++ Framework/ChunkLoadRegion.cs
@@ -6,13 +6,13 @@ namespace GalensUnified.CubicGrid.Framework;
 public class ChunkLoadRegion(Vector3D<int> centrePosition, int chunkLength, int halfLengthInChunks) : IChunkLoadRegion
 {
     public int ChunkLength => chunkLength;
-    private int _halfLengthInChunks;
+    private int _halfLengthInChunks = halfLengthInChunks;
     public int HalfLengthInChunks => _halfLengthInChunks;
-    private Vector3D<int> _centrePosition;
+    private Vector3D<int> _centrePosition = centrePosition;
     public Vector3D<int> CentrePosition => _centrePosition;
 
 
-    public Vector3D<int>[] chunks = [.. CubicNeighborhood.ExpandingCubePositions(centrePosition, new(halfLengthInChunks), chunkLength)];
+    public Vector3D<int>[] chunks = [.. CubicNeighborhood.ExpandingCubePositions(centrePosition, new(halfLengthInChunks * chunkLength), chunkLength)];
     public event Action<Vector3D<int>>? ChunkAdded;
     public event Action<Vector3D<int>>? ChunkRemoved;
 
@@ -30,7 +30,7 @@ public class ChunkLoadRegion(Vector3D<int> centrePosition, int chunkLength, int 
 
     private void UpdateManagedChunks()
     {
-        Vector3D<int>[] newPositions = [.. CubicNeighborhood.ExpandingCubePositions(CentrePosition, new(HalfLengthInChunks), chunkLength)];
+        Vector3D<int>[] newPositions = [.. CubicNeighborhood.ExpandingCubePositions(CentrePosition, new(HalfLengthInChunks * chunkLength), chunkLength)];
         List<Vector3D<int>> keysToRemove = [];
         foreach (Vector3D<int> pos in chunks)
             if (!newPositions.Contains(pos)) 
@@ -39,7 +39,8 @@ public class ChunkLoadRegion(Vector3D<int> centrePosition, int chunkLength, int 
         foreach (Vector3D<int> pos in keysToRemove)
             ChunkRemoved?.Invoke(pos);
         foreach (Vector3D<int> pos in newPositions)
-            ChunkAdded?.Invoke(pos);
+            if (!chunks.Contains(pos))
+                ChunkAdded?.Invoke(pos);
         chunks = newPositions;
     }
 }
